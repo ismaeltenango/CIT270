a1c4b60e888f633aeee47db2a7062bb70d43152b
ismaeltenango <ismael4477@gmail.com>
2023-05-05 13:51:29 -0600

    week3 final

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-const express = require('expres');
+const express = require('express');
 const app = express();
-app.listen(3000)
+const port = 3000
+
+app.listen(port, ()=> {
+    console.log("Listenning on port: " + port);
+});
+
+app.get('/', (req, res) => {
+    // res.send('Welcome to your server! No demons allowed')
+    res.redirect("https://www.byui.edu")
+})
