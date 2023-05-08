ee1d73d2d2dcc2612e9e199bfffa06bf02c92627
ismaeltenango <ismael4477@gmail.com>
2023-05-08 14:16:07 -0600

    Week 4.1

--- login.cmd
+++ login.cmd
@@ -1,5 +1,5 @@
-echo logging in
+echo "logging in"
 
-curl -v -d "@login.json" POST -H "Content-Type:applications/json" https://dev.stedi.me/login
+curl --insecure -d "@login.json" -X POST -H "Content-Type:application/json" http://localhost:3000/login
 
-curl https://dev.stedi.me/validate/5e14ff4b-d9ae-47f0-bba3-50e1a08f8656
+@REM curl https://dev.stedi.me/validate/5e14ff4b-d9ae-47f0-bba3-50e1a08f8656
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000
+const port = 3000;
+
+app.use(bodyParser.json()); //allow JSON (JvaScriptobject Notation) request
 
 app.listen(port, ()=> {
     console.log("Listenning on port: " + port);
 });
 
 app.get('/', (req, res) => {
-    // res.send('Welcome to your server! No demons allowed')
-    res.redirect("https://www.byui.edu")
+    res.send('Welcome to your server! No demons allowed');
+    // res.redirect("https://www.byui.edu");
 })
+
+app.post('/login', (req, res) => {
+    const loginBody = req.body;
+    const userName = loginBody.userName;
+    const password = loginBody.password;
+    if (password == "Ismael12345!"){
+        // THIS HAPPENDS IF THE PASWORD DOSES MATCH
+        res.send("Welcome " + userName);
+    } else {
+        // happens hen the password is not correct
+        // res.send("Incorrect Password");
+        res.send(req.body);
+    }
+    res.send("Welcome " + userName);
+});
