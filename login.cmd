e874888e41b4f3ecf9fba35f1502adc5d9fac3c4
ismaeltenango <ismael4477@gmail.com>
2023-06-14 13:29:04 -0600

    week9.2

--- login.cmd
+++ login.cmd
@@ -1,5 +1,5 @@
 echo "logging in"
 
-curl --insecure -d "@login.json" -X POST -H "Content-Type:application/json" http://localhost:3000/login
+curl --insecure -d "@login.json" -X POST -H "Content-Type:application/json" http://https://ismael-tenango.cit270.com/
 
 @REM curl https://dev.stedi.me/validate/5e14ff4b-d9ae-47f0-bba3-50e1a08f8656
